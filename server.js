2cc92cc9b46e029cfd1a4de9d4e941d5b6e7b678
Kagzi Md Izhan <izhankagzi313@gmail.com>
2026-09-07 23:57:38 +0530

    server prompt

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0",() => {
-  console.log(`🛒the Orbit Buy API running on http://localhost:${PORT}`);
+  console.log(`🛒  The Orbit Buy API running on http://localhost:${PORT}`);
 });
